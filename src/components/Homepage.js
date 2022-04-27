ba4482021c24d7daf4b7c22def75c41bfcabb80c
dann <dillydot22@gmail.com>
2022-04-27 11:01:08 +0100

    design changes

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -5,13 +5,13 @@ export const Homepage = () => {
     const navigate = useNavigate()
     return (
         <div style={{ 'position': 'relative' }}>
-            <h1 className="intro">Write out a thought with Notell!</h1>
+            <h1 className="intro">NOTELL!</h1>
+            <JournalText className="journal" width="50%" height="50%" color="#9e834b" />
             <ul>
                 <li className="list">Make Personal Notes</li>
                 <li className="list">A Feature Feed to let others see your notes!</li>
             </ul>
             <p style={{ 'color': '#faece2', 'margin': '30px' }}><a className="link-b" href="/login">Login</a> or <a className="link-b" href="/sign-up">Register</a> to get started.</p>
-            <JournalText className="journal" width="50%" height="50%" color="#9e834b" />
         </div >
     )
 }
