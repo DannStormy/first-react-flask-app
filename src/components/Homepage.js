dc9a266b32cb15c57acaba574e2dae95c488dd43
dann <dillydot22@gmail.com>
2022-04-27 13:13:29 +0100

    landing page design issues

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,16 +1,15 @@
 import { JournalText } from 'react-bootstrap-icons';
-import { useNavigate } from 'react-router-dom';
 
 export const Homepage = () => {
-    const navigate = useNavigate()
     return (
-        <div style={{}}>
+        <div className="homepage">
             <h1 className="intro">NOTELL!</h1>
+            <JournalText className='journal' />
             <ul>
-                <li className="list">Make Personal Notes</li>
+                <li className="list">Collect your thoughts!</li>
                 <li className="list">A Feature Feed to let others see your notes!</li>
             </ul>
-            <p style={{ 'color': '#faece2', 'margin': '30px' }}><a className="link-b" href="/login">Login</a> or <a className="link-b" href="/sign-up">Register</a> to get started.</p>
+            <p style={{ 'color': '#faece2', 'margin': '30px' }}><a className="link-b" href="/login">LOGIN</a> or <a className="link-b" href="/sign-up">REGISTER</a> to get started.</p>
         </div >
     )
 }
